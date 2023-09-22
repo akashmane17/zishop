4f33e95d601e68ef79919c1b19bb292af08aa87c
Akash Mane <akashmane277@gmail.com>
2023-09-22 11:06:05 +0530

    routes for products and errorHandler middleware

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from "express";
-import products from "./data/products.js";
 import connetDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import productRoutes from './routes/productRoutes.js';
 
 // connect to DB
 connetDB();
@@ -15,14 +16,9 @@ app.get("/", (req, res) => {
   res.send("API is running....");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products );
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use('/api/products', productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 
 app.listen(port, () => {
