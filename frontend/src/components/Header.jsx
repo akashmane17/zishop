3694259bb42c72b0bf2eb8075c37505ad3d70a6d
Akash Mane <akashmane277@gmail.com>
2023-09-23 10:22:01 +0530

    shows number of items in cart

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,14 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, Badge } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap";
+import { UseSelector, useSelector } from "react-redux/es/hooks/useSelector";
 
 import logo from "../assets/logo.png";
 
 const Header = () => {
+
+  const { cartItems } = useSelector((state) => state.cart);
+   
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
@@ -25,6 +29,10 @@ const Header = () => {
               <LinkContainer to="/cart">
                 <Nav.Link>
                   <FaShoppingCart /> Cart
+                  {/* Number of items in cart */}
+                  {cartItems.length > 0 && (
+                    <Badge pill bg='success' style={{marginLeft: '5px'}}>{cartItems.reduce((a, c) => a + c.qty, 0)}</Badge>
+                  )}
                 </Nav.Link>
               </LinkContainer>
 
