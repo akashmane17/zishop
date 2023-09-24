26b519e64a2d6e7fbfa96577b4f8ffa1caef54ef
Akash Mane <akashmane277@gmail.com>
2023-09-24 12:47:37 +0530

    new logo added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,12 +3,11 @@ import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap";
 import { UseSelector, useSelector } from "react-redux/es/hooks/useSelector";
 
-import logo from "../assets/logo.png";
+import logo from "../assets/z.png";
 
 const Header = () => {
-
   const { cartItems } = useSelector((state) => state.cart);
-   
+
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
@@ -16,7 +15,11 @@ const Header = () => {
           {/* Logo */}
           <LinkContainer to="/">
             <Navbar.Brand>
-              <img src={logo} alt="zishop" />
+              <img
+                src={logo}
+                alt="zishop"
+                style={{ width: "35px", marginRight: "5px" }}
+              />
               Zishop
             </Navbar.Brand>
           </LinkContainer>
@@ -31,7 +34,9 @@ const Header = () => {
                   <FaShoppingCart /> Cart
                   {/* Number of items in cart */}
                   {cartItems.length > 0 && (
-                    <Badge pill bg='success' style={{marginLeft: '5px'}}>{cartItems.reduce((a, c) => a + c.qty, 0)}</Badge>
+                    <Badge pill bg="success" style={{ marginLeft: "5px" }}>
+                      {cartItems.reduce((a, c) => a + c.qty, 0)}
+                    </Badge>
                   )}
                 </Nav.Link>
               </LinkContainer>
