6bb69c57447c39bd989d7efa6ed3129f85bdc116
Akash Mane <akashmane277@gmail.com>
2023-09-26 12:48:11 +0530

    setting up private routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,6 +18,7 @@ import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ShippingScreen from "./screens/ShippingScreen";
+import PrivateRoute from "./components/PrivateRoute";
 
 // All Routes
 const router = createBrowserRouter(
@@ -28,7 +29,11 @@ const router = createBrowserRouter(
       <Route path="/cart" element={<CartScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
-      <Route path="/shipping" element={<ShippingScreen />} />
+
+      {/* Private Routes */}
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/shipping" element={<ShippingScreen />} />
+      </Route>
     </Route>
   )
 );
