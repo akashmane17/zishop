4529441eac9166fd02edd1cca1b9bee7c988e146
Akash Mane <akashmane277@gmail.com>
2023-09-28 12:12:04 +0530

    feat: profile update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -23,6 +23,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
 // All Routes
 const router = createBrowserRouter(
@@ -40,6 +41,7 @@ const router = createBrowserRouter(
         <Route path="/payment" element={<PaymentScreen />} />
         <Route path="/placeorder" element={<PlaceOrderScreen />} />
         <Route path="/order/:id" element={<OrderScreen />} />
+        <Route path="/profile" element={<ProfileScreen />} />
       </Route>
     </Route>
   )
